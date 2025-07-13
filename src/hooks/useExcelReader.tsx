bab7bfbe9ed3bc651c7503694c4c3af055d9e02a
rena <raaguirre1@miuandes.cl>
2025-07-12 20:01:05 -0400

    feat: update Excel data fetching and add random color assignment

--- src/hooks/useExcelReader.tsx
+++ src/hooks/useExcelReader.tsx
@@ -3,19 +3,42 @@ import { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
 import Ramo from "../interfaces/Ramo";
 
+const colorNames300 = [
+  "red-300",
+  "pink-300",
+  "purple-300",
+  "violet-300",
+  "indigo-300",
+  "blue-300",
+  "sky-300",
+  "cyan-300",
+  "teal-300",
+  "emerald-300",
+  "green-300",
+  "lime-300",
+  "yellow-300",
+  "amber-300",
+  "orange-300",
+];
+
+function getRandomColor() {
+  return colorNames300[Math.floor(Math.random() * colorNames300.length)];
+}
+
 const useExcelReader = () => {
   const [ramos, setRamos] = useState<Ramo[]>([]);
+  
 
   useEffect(() => {
     const fetchExcel = async () => {
-      const response = await fetch("/TRA/HORARIO+ING_202510.xlsx");
+      const response = await fetch("/TRA/Horario_ING_202520.xlsx");
       const arrayBuffer = await response.arrayBuffer();
       const workbook = XLSX.read(new Uint8Array(arrayBuffer), {
         type: "array",
       });
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
-      //console.log(sheet);
+      console.log(sheet);
       const data: Ramo[] = XLSX.utils.sheet_to_json(sheet, {
         header: 1,
         defval: "",
@@ -23,7 +46,7 @@ const useExcelReader = () => {
       });
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const excelData = data.slice(13).map((row: any) => {
+      const excelData = data.slice(12).map((row: any) => {
         //console.log(row);
         // the slice(13) is to remove the first 13 rows of the excel file
         // console.log(row);
@@ -46,6 +69,7 @@ const useExcelReader = () => {
           sala: row[15],
           tipoDeReunion: row[16],
           profesor: row[17],
+          color: getRandomColor(),
         };
       });
 
